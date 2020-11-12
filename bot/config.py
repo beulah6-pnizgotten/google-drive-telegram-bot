38233cf6c61a08c042c5b9a8d951d31e38ad738b
beulah6-pnizgotten <74354277+beulah6-pnizgotten@users.noreply.github.com>
2020-11-12 16:47:27 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,7 +2,7 @@ class config:
     BOT_TOKEN = "1400384913:AAHqy6Bj6ADiS6m8u_36BGzXKndQKessaw8"
     APP_ID = "1389947"
     API_HASH = "869411ef20ea0506b87016510df54ba5"
-    DATABASE_URL = "postgres://mafznxrtpebecc:71cf1b046e94794569b48a8d682dd12dc0f879880633d53af8ff30b2005b0151@ec2-3-208-224-152.compute-1.amazonaws.com:5432/d39aognge62jeb"
+    DATABASE_URL = "postgres://lfbtwphooctauj:cb9d91f99b812be48b406145efae5e64e30bf18252cea21a8f80f08345402227@ec2-54-82-208-124.compute-1.amazonaws.com:5432/d6ofbv2jktji8c"
     SUDO_USERS = "842544598 1026084657 700355752" # Sepearted by space.
     SUPPORT_CHAT_LINK = "https://t.me/yt_DarkHackerX_chat"
     DOWNLOAD_DIRECTORY = "./downloads/"
